646c4a051d24f87e10d77222e5b64fd966fddc7a
Luka Koridze <luka.qoridze2002@gmail.com>
2022-10-08 15:13:29 +0400

    Finish

---
+++ main.js
@@ -0,0 +1,33 @@
+let circle = document.querySelectorAll('.circle')
+let bar = document.querySelectorAll('.bar')
+let width = bar[0].getBoundingClientRect().width
+let x = bar[0].getBoundingClientRect().left
+let barGreen = '#A4F3EB';
+let barGrey = '#ECF0FB';
+circle.forEach((draggable) => {
+    draggable.addEventListener('mousedown', () => {
+        document.addEventListener('mousemove', drag);
+        draggable.style.backgroundColor = '#24AEA1'
+    })
+})
+function drag(event) {
+    let left = event.clientX - x
+    let perc = Math.round(left / width * 1000) / 10
+    if (left < 0) {
+        left = 0
+    } else if (left > width) {
+        left = width
+    }
+    circle.forEach((cir) => {
+        cir.style.left = `${left}px`
+    })
+    bar.forEach((bar) => {
+        bar.style.background = `linear-gradient(to right, ${barGreen} ${perc - 0.1}%, ${barGrey} ${perc}%)`
+    })
+}
+document.addEventListener('mouseup', () => {
+    document.removeEventListener('mousemove', drag);
+    circle.forEach((draggable) => {
+        draggable.style.backgroundColor = '#7AEADF'
+    })
+})
